ba3455a2c253fa2056387e67c7fb75ec0bffb7ae
neha <neha.ann.dev@gmail.com>
2025-01-18 11:46:41 -0800

    Initial Commit

---
+++ EscapeRoom/EscapeRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeRoomApp.swift
+//  EscapeRoom
+//
+//  Created by Neha Arun on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct EscapeRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EscapeRoomTests/EscapeRoomTests.swift
@@ -0,0 +1,16 @@
+//
+//  EscapeRoomTests.swift
+//  EscapeRoomTests
+//
+//  Created by Neha Arun on 1/18/25.
+//
+
+import Testing
+
+struct EscapeRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
