acd80f93e3eef50f001b5d4e66577e0380ff49c4
neha <neha.ann.dev@gmail.com>
2025-01-18 19:04:58 -0800

    seed commit

--- EscapeRoom/ContentView.swift
+++ EscapeRoom/ContentView.swift
@@ -1,24 +1,72 @@
-//
-//  ContentView.swift
-//  EscapeRoom
-//
-//  Created by Neha Arun on 1/18/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct LaunchScreen: View {
+    @State private var codeInput = "" // Stores the user input
+    @State private var message = "Find the code to escape!"
+    @State private var isDoorUnlocked = false
+
+    var body:  some View {
+        ZStack {
+            // Background
+            Image("roomBackground")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 20) {
+                // Feedback message
+                Text(message)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+
+                // Input field for the code
+                TextField("Enter the code", text: $codeInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .frame(width: 200)
+
+                // Button to check the code
+                Button(action: checkCode) {
+                    Text("Unlock Box")
+                        .font(.headline)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                // Door navigation
+                if isDoorUnlocked {
+                    NavigationLink(destination: NextRoomView()) {
+                        Text("Instructor")
+                            .font(.title)
+                            .padding()
+                            .background(Color.teal)
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+
+    // Move the checkCode function here
+    func checkCode() {
+        if codeInput == "75561" { // Correct code
+            message = "The box is unlocked! You can proceed."
+            isDoorUnlocked = true
+        } else {
+            message = "Incorrect code. Try again!"
+        }
     }
 }
 
-#Preview {
-    ContentView()
+// Move the NextRoomView struct outside the body
+struct NextRoomView: View {
+    var body: some View {
+        Text("Welcome to the next room!")
+            .font(.largeTitle)
+            .foregroundColor(.green)
+    }
 }
--- EscapeRoom/EscapeRoomApp.swift
+++ EscapeRoom/EscapeRoomApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct EscapeRoomApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                LaunchScreen()
+            }
         }
     }
 }
